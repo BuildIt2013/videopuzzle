dc6d3265818706c0a223b2e45f27338c6a6d4211
Vidhuran <vidhuran2012@gmail.com>
2013-09-14 01:21:20 +0300

    trying to add image to TextBlock from MediaElement

--- videopuzzle/MainPage.xaml.cs
+++ videopuzzle/MainPage.xaml.cs
@@ -5,25 +5,45 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Windows.Media.Imaging;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using videopuzzle.Resources;
+using System.Windows.Documents;
 
 namespace videopuzzle
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private MediaElement mediaElement;
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-            MediaElement mediaElement = new MediaElement();
+            mediaElement = new MediaElement();
             mediaElement.Source = new Uri("/Assets/Videos/gokarts.mp4", UriKind.Relative);
             mediaElement.Volume = 100;
             mediaElement.Play();
-
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+        }
+
+        private void textBlock11_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            // Create a WriteableBitmap and set it to the MediaElement (video).
+            // The WriteableBitmap represents a "snapshot" of the video.
+            WriteableBitmap wb = new WriteableBitmap(mediaElement, null);
+
+            // Create an image of the desired size and set its source to
+            // the WriteableBitmap representing a snapshot of the video.
+            Image image = new Image();
+            image.Height = 150;
+            image.Margin = new Thickness(10);
+            image.Source = wb;
+
+            InlineUIContainer iuc = new InlineUIContainer();
+            iuc.Child = image;
+            textBlock11.Inlines.Add(iuc);
         }
 
         // Sample code for building a localized ApplicationBar
