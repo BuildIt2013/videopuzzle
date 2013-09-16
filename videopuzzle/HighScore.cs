80b1d405a41f906af9a718e3f54894d3b603d118
Tuomo Vierros <tuomo.vierros@gmail.com>
2013-09-17 01:10:55 +0300

    added missing database connection files

---
+++ videopuzzle/HighScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videopuzzle
+{
+    class HighScore
+    {
+        public int Id { get; set; }
+        public int Time { get; set; }
+        public int PuzzleId { get; set; }
+        public int PlayerId { get; set; }
+    }
+
+}
---
+++ videopuzzle/Puzzle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videopuzzle
+{
+    class Puzzle
+    {
+        public int Id { get; set; }
+        public string ImageUri { get; set; }
+        public int[] InitialPosition { get; set; }
+        public string ImageCredits { get; set; }
+    }
+}
